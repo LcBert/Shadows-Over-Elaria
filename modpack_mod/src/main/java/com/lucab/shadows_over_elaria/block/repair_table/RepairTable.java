fa1c8635b9898ddf427dd29f0d7fa90b1744fe39
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-03-03 19:18:29 +0100

    Implement repair table functionality with item interaction and inventory management

--- modpack_mod/src/main/java/com/lucab/shadows_over_elaria/block/repair_table/RepairTable.java
+++ modpack_mod/src/main/java/com/lucab/shadows_over_elaria/block/repair_table/RepairTable.java
@@ -1,9 +1,17 @@
 package com.lucab.shadows_over_elaria.block.repair_table;
 
+import com.lucab.shadows_over_elaria.Utils;
 import com.mojang.serialization.MapCodec;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.SoundType;
@@ -11,6 +19,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -30,6 +39,54 @@ public class RepairTable extends BaseEntityBlock {
                 .strength(3.5F)
                 .requiresCorrectToolForDrops()
                 .noOcclusion());
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos,
+            Player player, InteractionHand hand, BlockHitResult hitResult) {
+
+        RepairTableEntity table = (RepairTableEntity) level.getBlockEntity(pos);
+        if (table == null)
+            return ItemInteractionResult.FAIL;
+
+        if (!player.isShiftKeyDown()) {
+            if (Utils.isRepairKit(stack)) {
+                ItemStack remainder = table.insertKit(stack);
+                if (remainder.getCount() != stack.getCount())
+                    player.setItemInHand(hand, remainder);
+            } else if (Utils.isItemToRepair(stack)) {
+                ItemStack remainder = table.insertItem(stack);
+                if (remainder.getCount() != stack.getCount())
+                    player.setItemInHand(hand, remainder);
+            } else if (Utils.isRepairHammer(stack)) {
+                if (table.repair(level, pos, state)) {
+                    stack.hurtAndBreak(1, player, EquipmentSlot.MAINHAND);
+                }
+            }
+        } else {
+            if (table.hasItem()) {
+                ItemStack removedItem = table.removeItem();
+                if (player.getInventory().add(removedItem))
+                    player.drop(removedItem, false);
+            } else if (table.hasKit()) {
+                ItemStack removedKit = table.removeKit();
+                if (player.getInventory().add(removedKit))
+                    player.drop(removedKit, false);
+            }
+        }
+
+        return ItemInteractionResult.SUCCESS;
+    }
+
+    @Override
+    protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
+        RepairTableEntity repairTable = (RepairTableEntity) level.getBlockEntity(pos);
+        for (int i = 0; i < repairTable.inventory.getSlots(); i++) {
+            Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(),
+                    repairTable.inventory.getStackInSlot(i));
+        }
+
+        super.onRemove(state, level, pos, newState, movedByPiston);
     }
 
     @Override
